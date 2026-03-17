5d1adedd93e73d29d9bad5a0df72719e253f3c3c
dev-woong-92 <dev.woong.92@gmail.com>
2026-03-17 20:46:44 +0900

    feat: add src/pages/Jk/Jk.tsx

---
+++ src/pages/Jk/Jk.tsx
@@ -0,0 +1,9 @@
+export default function HeroText() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-9xl font-black text-gray-900 tracking-widest select-none">
+        JK
+      </h1>
+    </div>
+  );
+}
