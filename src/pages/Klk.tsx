51ee6d0d43d510f890dee77a5647bb401b05d248
dev-woong-92 <dev.woong.92@gmail.com>
2026-03-18 14:20:51 +0900

    feat: add src/pages/Klk.tsx

---
+++ src/pages/Klk.tsx
@@ -0,0 +1,9 @@
+export default function Klk() {
+  return (
+    <main className="bg-black min-h-screen flex items-center justify-center">
+      <h1 className="text-red-500 text-8xl font-bold text-center animate-pulse tracking-wider">
+        무섭다
+      </h1>
+    </main>
+  );
+}
